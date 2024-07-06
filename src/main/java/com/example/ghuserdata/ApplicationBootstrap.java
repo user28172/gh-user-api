5e5db4d61933d1c350c208bff4d49fb00fc2f8e4
TF <sample@domain.com>
2024-07-07 00:24:25 +0200

    init

---
+++ src/main/java/com/example/ghuserdata/ApplicationBootstrap.java
@@ -0,0 +1,13 @@
+package com.example.ghuserdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBootstrap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBootstrap.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ghuserdata/ApplicationBootstrapTests.java
@@ -0,0 +1,13 @@
+package com.example.ghuserdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationBootstrapTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
